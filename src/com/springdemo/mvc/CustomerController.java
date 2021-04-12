65edd5a5f8e752f5f72ad91e39c7810a0e25e4cb
Anshika15 <bansalanshika20@gmail.com>
2021-04-12 14:16:44 +0530

    Pre-processing Code with InitBinder

--- src/com/springdemo/mvc/CustomerController.java
+++ src/com/springdemo/mvc/CustomerController.java
@@ -1,16 +1,13 @@
 package com.springdemo.mvc;
 
-//import javax.validation.Valid;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 
 
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,6 +16,17 @@ import javax.validation.Valid;
 @Component
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	// add an initbinder ... to convert trim input strings
+	// remove trailing and leading spaces
+	// resolve issue for our validation
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder)
+	{
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
 
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
