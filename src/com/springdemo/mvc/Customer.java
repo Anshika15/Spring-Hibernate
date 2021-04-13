a70ee51a3a2ab7b6a97c3d40a630544ecf4b6315
Anshika15 <bansalanshika20@gmail.com>
2021-04-13 18:20:50 +0530

    Applying regular expressions

--- src/com/springdemo/mvc/Customer.java
+++ src/com/springdemo/mvc/Customer.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.Size;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 public class Customer {
 
 	private String firstName;
@@ -16,6 +17,17 @@ public class Customer {
 	@Max(value=10, message="must be less than or equal to 10")
 	private int freePasses;
 	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
