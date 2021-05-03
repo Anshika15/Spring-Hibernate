36081dc26c8551d8c96656f443305edf2ab9cee0
Anshika15 <bansalanshika20@gmail.com>
2021-05-03 16:26:58 +0530

    Prep Work

---
+++ src/com/practice/jdbc/TestJdbc.java
@@ -0,0 +1,24 @@
+package com.practice.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
+		String user = "root";
+		String pwd = " ";
+		try {
+			System.out.println("Connecting to Databse " + jdbcUrl);
+			 Class.forName("com.mysql.jdbc.Driver");
+			 Connection conn = DriverManager.getConnection(jdbcUrl, user, pwd);
+			
+			System.out.println("Connection Successful");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
