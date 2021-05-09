515d6317764d11e55d8ecef7ebcb0812a3505551
ANSHIKA <bansalanshika20@gmail.com>
2021-05-09 16:42:37 +0530

    Get Courses and Reviews

---
+++ src/com/practice/hibernate/demo/GetCourseAndReviewsDemo.java
@@ -0,0 +1,52 @@
+package com.practice.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.practice.hibernate.demo.entity.Course;
+import com.practice.hibernate.demo.entity.Instructor;
+import com.practice.hibernate.demo.entity.InstructorDetail;
+import com.practice.hibernate.demo.entity.Review;
+import com.practice.hibernate.demo.entity.Student;
+
+public class GetCourseAndReviewsDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class).buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			// get the course
+			int theId = 10;
+			Course tempCourse = session.get(Course.class, theId);
+
+			// print the course
+			System.out.println(tempCourse);
+
+			// print the course reviews
+			System.out.println(tempCourse.getReviews());
+
+			// commit transaction
+			session.getTransaction().commit();
+			System.out.println("Work done!");
+
+		} finally {
+			// add clean up code
+			session.close();
+
+			factory.close();
+		}
+
+	}
+
+}
