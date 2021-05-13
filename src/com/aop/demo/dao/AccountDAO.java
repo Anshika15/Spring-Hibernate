21c1d9819e36a63e6dd2c2b367e17f36b5e8efa1
ANSHIKA <bansalanshika20@gmail.com>
2021-05-13 17:29:34 +0530

    Create Target object

---
+++ src/com/aop/demo/dao/AccountDAO.java
@@ -0,0 +1,12 @@
+package com.aop.demo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+	
+	public void addAccount() {
+		System.out.println(getClass() + ": Doing my db Work: Adding an account");
+	}
+
+}
