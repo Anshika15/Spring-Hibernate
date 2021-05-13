ba08f982b0457d8b00149662151da82d4e6afae3
ANSHIKA <bansalanshika20@gmail.com>
2021-05-13 21:25:24 +0530

    Pointcut Expressions - Match only DAO addAccount

--- src/com/aop/demo/MainDemoApp.java
+++ src/com/aop/demo/MainDemoApp.java
@@ -3,31 +3,36 @@ package com.aop.demo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.aop.demo.dao.AccountDAO;
+import com.aop.demo.dao.MembershipDAO;
 
 public class MainDemoApp {
 
 	public static void main(String[] args) {
-		
+
 		// read spring config java class
-		AnnotationConfigApplicationContext context =
-				new AnnotationConfigApplicationContext(DemoConfig.class);
-		
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
 		// get the bean from spring container
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
-		
+
+		// get the membership bean from spring container
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
 		// call the business method
 		theAccountDAO.addAccount();
-		
-		// let's try it again!
-		System.out.println("\nLet's call it again");
-		
-		// call the business method again
-		theAccountDAO.addAccount();
-		
+
+		// call the membership business method
+		theMembershipDAO.addAccount();
+
+//		// let's try it again!
+//		System.out.println("\nLet's call it again");
+//
+//		// call the business method again
+//		theAccountDAO.addAccount();
+
 		// close the context
 		context.close();
-		
-		
+
 	}
 
 }
